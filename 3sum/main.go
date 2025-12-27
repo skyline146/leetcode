dac0967f745d49a4e6f811ef6539a73c1cffc6db
Bukatsel,Dmitriy <dmitriy.bukatsel@simpleone.ru>
2025-12-27 15:36:57 +0300

    updated 3sum

--- 3sum/main.go
+++ 3sum/main.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
-func twoSum(nums []int, target int) [][2]int {
-	res := make([][2]int, 0)
-
+func twoSum(nums []int, target int) [][]int {
+	res := make([][]int, 0)
 	m := make(map[int][]int)
+
 	for i, v := range nums {
 		if a, ok := m[target-v]; ok {
 			for _, j := range a {
-				res = append(res, [2]int{j, i})
+				minv, maxv := min(nums[i], nums[j]), max(nums[i], nums[j])
+
+				res = append(res, []int{minv, maxv})
 			}
 		}
 
@@ -21,19 +26,26 @@ func twoSum(nums []int, target int) [][2]int {
 
 func threeSum(nums []int) [][]int {
 	res := make([][]int, 0)
-	m := make(map[int][]int)
+	slices.Sort(nums)
 
-	for i := 0; i < len(nums); i++ {
-		if _, ok := m[nums[i]]; !ok {
-			m[nums[i]] = make([]int, 0)
+	for i := 0; i < len(nums)-2; i++ {
+		v := nums[i]
+
+		if i != 0 && v == nums[i-1] {
+			continue
 		}
 
-		m[nums[i]] = append(m[nums[i]], i)
+		for _, k := range twoSum(nums[i+1:], -v) {
+			res = append(res, append(k, v))
+		}
 	}
 
 	return res
 }
 
 func main() {
-	fmt.Println(twoSum([]int{-1, 0, 1, 2, -1, -4, 1, 1, 0, 1}, 2))
+	// fmt.Println(twoSum([]int{0, 1, 2, -1, 1, -4}, 1))
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	// fmt.Println(threeSum([]int{-100000, -99999, 199999}))
+	// fmt.Println(threeSum([]int{0, 0, 0}))
 }
